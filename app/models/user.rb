0b2693c4790fc71db6cbfe644c111fa2c5553ddc
anderojr <and.okuda@gmail.com>
2024-09-08 01:36:16 -0300

    fixed logic to use username instead of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :rememberable, :validatable, authentication_keys: [:username]
+         :rememberable, authentication_keys: [:username]
   
   validates :username, presence: true, uniqueness: true
-  validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, presence: true, on: [:create, :update]
 end
